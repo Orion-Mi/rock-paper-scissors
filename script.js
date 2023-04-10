e7eed2c94940dd84b0d6dab7c050515268b6a522
Orion-Mi <danielopio98@gmail.com>
2023-04-10 05:57:07 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+/*function getComputerChoice(rock, paper, scissors){
+    let getComputerChoice = Math.floor(Math.random() * 3);
+    console.log(getComputerChoice);
+}*/
+const Choices = ['rock', 'paper', 'scissors'];
+function getComputerChoice() {
+    getComputerChoice = Math.floor(Math.random() * 3);
+    return(getComputerChoice);
+}
+function playRound(playerChoice, computerChoice){
+    playerChoice = Choices[0];
+    if (computerChoice = playerChoice){
+        console.log('It\'s a tie');
+    }else if (computerChoice = 1){
+        console.log('Computer wins!');
+    }else {
+        console.log('You win!');
+    }
+}
